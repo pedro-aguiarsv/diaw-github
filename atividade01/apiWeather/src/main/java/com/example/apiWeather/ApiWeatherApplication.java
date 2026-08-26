5e29abc86e89c4804bfbe14aead05c925864ee14
pedro-aguiarsv <pedro.asvgouvea@gmail.com>
2026-08-26 17:42:29 -0300

    primeiro push

---
+++ atividade01/apiWeather/src/main/java/com/example/apiWeather/ApiWeatherApplication.java
@@ -0,0 +1,16 @@
+package com.example.apiWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeatherApplication.class, args);
+	}
+
+}
+
+
+https://geocoding-api.open-meteo.com/v1
---
+++ atividade01/apiWeather/src/test/java/com/example/apiWeather/ApiWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
